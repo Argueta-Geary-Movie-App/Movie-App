47c8ef3b95902e7dd355cdb504e79a98183f6f9b
Casanova Geary <CasanovaGeary@Gmail.com>
2022-09-20 15:55:51 -0500

    Started JS Event Listeners.

--- main.js
+++ main.js
@@ -22,6 +22,17 @@ function fetchData() {
 fetchData();
 
 // Event Listeners
+$('.find-movie').on(click(function () {
 
+}))
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
 
 // HTML
